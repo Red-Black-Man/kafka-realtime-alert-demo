5af4d1c808ea7e29feb0789b46ab5740c0b4272d
Red-Black-Man <55904517+Red-Black-Man@users.noreply.github.com>
2025-12-20 17:48:04 +0800

    Update AlertConsumer.java

--- AlertConsumer.java
+++ AlertConsumer.java
@@ -1,16 +1,20 @@
 package com.example.demo;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+
 @Service
 public class AlertConsumer {
-private final SimpMessagingTemplate webSocketTemplate;
-public AlertConsumer(SimpMessagingTemplate webSocketTemplate) {
-this.webSocketTemplate = webSocketTemplate;
-}
-@KafkaListener(topics = "alerts", groupId = "alert-group", containerFactory = "kafkaListenerContainerFactory")
-public void consumeAlert(String alertMessage) {
-System.out.println("Received alert: " + alertMessage);
-webSocketTemplate.convertAndSend("/topic/alerts", alertMessage);
-}
+    private final SimpMessagingTemplate webSocketTemplate;
+
+    public AlertConsumer(SimpMessagingTemplate webSocketTemplate) {
+        this.webSocketTemplate = webSocketTemplate;
+    }
+
+    @KafkaListener(topics = "alerts", groupId = "alert-group", containerFactory = "kafkaListenerContainerFactory")
+    public void consumeAlert(String alertMessage) {
+        System.out.println("Received alert: " + alertMessage);
+        webSocketTemplate.convertAndSend("/topic/alerts", alertMessage);
+    }
 }
